b332e120f396928fe324df2dd76cd5f2d294a7a1
João Gabryel <sjoaogabryel@gmail.com>
2025-09-30 21:59:03 -0300

    fix/correção de transação

--- MVP/back-end/app/Http/Controllers/TransacaoController.php
+++ MVP/back-end/app/Http/Controllers/TransacaoController.php
@@ -37,7 +37,7 @@ class TransacaoController extends Controller
             'categoria'       => 'required|string|min:2|max:100',
             'descricao'       => 'required|string|min:3|max:255',
             'forma_pagamento' => 'required|string',
-            'situacao'        => 'required|in:Pendente,Concluída,Cancelada',
+            'situacao'        => 'required|in:cendente,concluida,cancelada',
             'observacao'      => 'nullable|string|max:1000',
         ]);
 
